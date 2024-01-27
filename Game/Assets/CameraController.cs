3fc06d2b598ae7e62973c5dc0c7f2df316be4d0f
Tuizku <tuisku.kahra@gmail.com>
2024-01-27 18:46:31 +0200

    fix with merge

--- Game/Assets/CameraController.cs
+++ Game/Assets/CameraController.cs
@@ -5,25 +5,11 @@ using UnityEngine.Events;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float camRotationSpeed = 15f;
-    [SerializeField] private float rotationAngle;
-
-    [SerializeField] private UnityEvent<int> OnGravityChange;
-
+    [SerializeField] private float rotationSpeed = 15f;
 
     private void Update()
     {
-        // Change the gravity when "a" or "d" is down
-        if (Input.GetKeyDown(KeyCode.A)) ChangeGravity(-90);
-        if (Input.GetKeyDown(KeyCode.D)) ChangeGravity(90);
-
         // Rotate the camera smoothly, based on the rotationDegrees
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, rotationAngle), camRotationSpeed * Time.deltaTime);
-    }
-
-    private void ChangeGravity(float addedAngle)
-    {
-        rotationAngle = (rotationAngle + addedAngle) % 360;
-        OnGravityChange.Invoke(Mathf.RoundToInt(rotationAngle));
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, GravityController.globalGravityAngle), rotationSpeed * Time.deltaTime);
     }
 }
